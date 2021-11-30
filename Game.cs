b4795dfed1cd7e97a724c0eff0fe57fd3883bb1f
JarousNemec <jarous.nemec@gmail.com>
2021-11-30 10:48:38 +0100

    Refactored code

--- Game.cs
+++ Game.cs
@@ -8,11 +8,11 @@ using System.IO;
 
 namespace ConsoleApp1
 {
-    public class gameData
+    public class GameData
     {
-        private static int mapSizeX = 50;
+        private static int mapSizeX = 10;
         private static int mapSizeY = mapSizeX;
-        public cell[,] map = new cell[mapSizeX, mapSizeY];
+        public Field[,] map = new Field[mapSizeX, mapSizeY];
         public string[,] display = new string[mapSizeX, mapSizeY];
         public int cursorX = 0;
         public int cursorY = 0;
@@ -20,8 +20,7 @@ namespace ConsoleApp1
         public int mineCount = 0;
         public int originalCountOfMines = 0;
     }
-
-    public class cell
+    public class Field
     {
         public bool isMine = false;
         public bool isChecked = false;
@@ -29,174 +28,221 @@ namespace ConsoleApp1
     }
     public class Game
     {
-
         public Game()
         {
-            gameData data = new gameData();
-
-            generateMap(data);
-
-            fillEmptyDisplay(data);
-            RenderMap(data);
-
-            bool run = true;
-
-            while (run)
-            {
-                RenderMap(data);
-                ConsoleKeyInfo key = Console.ReadKey();
-                switch (key.Key)
-                {
-                    case (ConsoleKey)13:
-                        {
-                            if (data.map[data.cursorX, data.cursorY].around >= 8)
-                            {
-                                run = false;
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Game Over");
-                            }
-                            if (data.map[data.cursorX, data.cursorY].around < 8)
-                            {
-                                data.display[data.cursorX, data.cursorY] = Convert.ToString(data.map[data.cursorX, data.cursorY].around);
-                            }
-
-                        }
-                        break;
-                    case (ConsoleKey)32:
-                        {
-                            if (data.map[data.cursorX, data.cursorY].isChecked)
-                            {
-                                if (data.mineChecked < data.originalCountOfMines)
-                                {
-                                    data.mineChecked += 1;
-                                    if (data.map[data.cursorX, data.cursorY].isMine)
-                                    {
-                                        data.mineCount += 1;
-                                    }
-                                    data.map[data.cursorX, data.cursorY].isChecked = false;
-                                }
-                            }
-                            else
-                            {
-                                if (data.mineChecked > 0)
-                                {
-                                    data.mineChecked -= 1;
-                                    if (data.map[data.cursorX, data.cursorY].isMine)
-                                    {
-                                        data.mineCount -= 1;
-                                    }
-                                    data.map[data.cursorX, data.cursorY].isChecked = true;
-                                }
-                            }
-
-                        }
-                        break;
-                    case (ConsoleKey)37:
-                        {
-                            if (data.cursorX - 1 >= 0)
-                            {
-                                data.cursorX -= 1;
-                            }
-                        }
-                        break;
-                    case (ConsoleKey)38:
-                        {
-                            if (data.cursorY - 1 >= 0)
-                            {
-                                data.cursorY -= 1;
-                            }
-                        }
-                        break;
-                    case (ConsoleKey)39:
-                        {
-                            if (data.cursorX + 1 < data.display.GetLength(0))
-                            {
-                                data.cursorX += 1;
-                            }
-                        }
-                        break;
-                    case (ConsoleKey)40:
-                        if (data.cursorY + 1 < data.display.GetLength(1))
-                        {
-                            data.cursorY += 1;
-                        }
-                        break;
-                }
-
-                if (data.mineCount == 0)
-                {
-                    run = false;
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("You WON!!!!!!!!!!");
-                }
-            }
-
-
-
+            GameData data = new GameData();
+            Play(data);
             Console.ReadKey();
         }
-
-        private void generateMap(gameData data)
+        private void Play(GameData data)
         {
-            fillEmptyMap(data);
+            GenerateMap(data);
+            FillEmptyDisplay(data);
+            RenderMap(data);
+            bool isGameRunning = true;
+            while (isGameRunning)
+            {
+                RenderMap(data);
+                isGameRunning = KeyController(data);
+                if (data.mineCount == 0)
+                {
+                    isGameRunning = false;
+                    WinningScreen();
+                }
+            }
+        }
+        private static void WinningScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You WON!!!!!!!!!!");
+        }
+        private static bool KeyController(GameData data)
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            switch (key.Key)
+            {
+                case (ConsoleKey) 13:
+                    if (!UncoverField(data)) return false;
+                    break;
+                case (ConsoleKey) 32:
+                    MarkField(data);
+                    break;
+                case (ConsoleKey) 37:
+                    MoveCursorLeft(data);
+                    break;
+                case (ConsoleKey) 38:
+                    MoveCursorUp(data);
+                    break;
+                case (ConsoleKey) 39:
+                    MoveCursorRight(data);
+                    break;
+                case (ConsoleKey) 40:
+                    MoveCursorDown(data);
+                    break;
+            }
+            return true;
+        }
+        private static bool UncoverField(GameData data)
+        {
+            if (data.map[data.cursorX, data.cursorY].around >= 8)
+            {
+                DefeatScreen();
+                return false;
+            }
+
+            if (data.map[data.cursorX, data.cursorY].around < 8)
+            {
+                data.display[data.cursorX, data.cursorY] =
+                    Convert.ToString(data.map[data.cursorX, data.cursorY].around);
+            }
+
+            return true;
+        }
+        private static void DefeatScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Game Over");
+        }
+        private static void MarkField(GameData data)
+        {
+            if (data.map[data.cursorX, data.cursorY].isChecked)
+            {
+                if (data.mineChecked < data.originalCountOfMines)
+                {
+                    data.mineChecked += 1;
+                    if (data.map[data.cursorX, data.cursorY].isMine)
+                    {
+                        data.mineCount += 1;
+                    }
+
+                    data.map[data.cursorX, data.cursorY].isChecked = false;
+                }
+            }
+            else
+            {
+                if (data.mineChecked > 0)
+                {
+                    data.mineChecked -= 1;
+                    if (data.map[data.cursorX, data.cursorY].isMine)
+                    {
+                        data.mineCount -= 1;
+                    }
+
+                    data.map[data.cursorX, data.cursorY].isChecked = true;
+                }
+            }
+        }
+        private static void MoveCursorLeft(GameData data)
+        {
+            if (data.cursorX - 1 >= 0)
+            {
+                data.cursorX -= 1;
+            }
+        }
+        private static void MoveCursorUp(GameData data)
+        {
+            if (data.cursorY - 1 >= 0)
+            {
+                data.cursorY -= 1;
+            }
+        }
+        private static void MoveCursorRight(GameData data)
+        {
+            if (data.cursorX + 1 < data.display.GetLength(0))
+            {
+                data.cursorX += 1;
+            }
+        }
+        private static void MoveCursorDown(GameData data)
+        {
+            if (data.cursorY + 1 < data.display.GetLength(1))
+            {
+                data.cursorY += 1;
+            }
+        }
+        private void GenerateMap(GameData data)
+        {
+            FillEmptyMap(data);
+
             Random r = new Random();
-            float itemID;
+
             for (int i = 0; i < data.map.GetLength(0); i++)
             {
                 for (int j = 0; j < data.map.GetLength(1); j++)
                 {
-                    itemID = r.Next(0, 10);
-                    if (itemID == 8)
-                    {
-                        data.map[i, j].around = 8;
-                        data.map[i, j].isMine = true;
-                        data.mineCount += 1;
-
-                        if ((i - 1) >= 0)
-                        {
-                            data.map[i - 1, j].around += 1;
-                        }
-                        if ((i + 1) < data.map.GetLength(0))
-                        {
-                            data.map[i + 1, j].around += 1;
-                        }
-                        if ((j - 1) >= 0)
-                        {
-                            data.map[i, j - 1].around += 1;
-                        }
-                        if ((j + 1) < data.map.GetLength(1))
-                        {
-                            data.map[i, j + 1].around += 1;
-                        }
-                        if ((i - 1) >= 0 && (j - 1) >= 0)
-                        {
-                            data.map[i - 1, j - 1].around += 1;
-                        }
-                        if ((i + 1) < data.map.GetLength(0) && (j + 1) < data.map.GetLength(1))
-                        {
-                            data.map[i + 1, j + 1].around += 1;
-                        }
-                        if ((i + 1) < data.map.GetLength(0) && (j - 1) >= 0)
-                        {
-                            data.map[i + 1, j - 1].around += 1;
-                        }
-                        if ((i - 1) >= 0 && (j + 1) < data.map.GetLength(1))
-                        {
-                            data.map[i - 1, j + 1].around += 1;
-                        }
-                    }
-                    else
-                    {
-                        data.map[i, j].around += 0;
-                    }
+                    InitializeNewField(data, r, i, j);
                 }
             }
 
             data.mineChecked = data.mineCount;
             data.originalCountOfMines = data.mineCount;
         }
+        private static void InitializeNewField(GameData data, Random r, int i, int j)
+        {
+            int itemId = r.Next(0, 10);
+            if (itemId == 8)
+            {
+                CreateMine(data, i, j);
+            }
+            else
+            {
+                CreateEmptyField(data, i, j);
+            }
+        }
+        private static void CreateEmptyField(GameData data, int i, int j)
+        {
+            data.map[i, j].around += 0;
+        }
+        private static void CreateMine(GameData data, int i, int j)
+        {
+            data.map[i, j].around = 8;
+            data.map[i, j].isMine = true;
+            data.mineCount += 1;
+            IncreaseValueByOneInFieldsAroundMine(data, i, j);
+        }
+        private static void IncreaseValueByOneInFieldsAroundMine(GameData data, int i, int j)
+        {
+            if ((i - 1) >= 0)
+            {
+                data.map[i - 1, j].around += 1;
+            }
 
-        private void fillEmptyDisplay(gameData data)
+            if ((i + 1) < data.map.GetLength(0))
+            {
+                data.map[i + 1, j].around += 1;
+            }
+
+            if ((j - 1) >= 0)
+            {
+                data.map[i, j - 1].around += 1;
+            }
+
+            if ((j + 1) < data.map.GetLength(1))
+            {
+                data.map[i, j + 1].around += 1;
+            }
+
+            if ((i - 1) >= 0 && (j - 1) >= 0)
+            {
+                data.map[i - 1, j - 1].around += 1;
+            }
+
+            if ((i + 1) < data.map.GetLength(0) && (j + 1) < data.map.GetLength(1))
+            {
+                data.map[i + 1, j + 1].around += 1;
+            }
+
+            if ((i + 1) < data.map.GetLength(0) && (j - 1) >= 0)
+            {
+                data.map[i + 1, j - 1].around += 1;
+            }
+
+            if ((i - 1) >= 0 && (j + 1) < data.map.GetLength(1))
+            {
+                data.map[i - 1, j + 1].around += 1;
+            }
+        }
+        private void FillEmptyDisplay(GameData data)
         {
             for (int i = 0; i < data.display.GetLength(0); i++)
             {
@@ -206,18 +252,17 @@ namespace ConsoleApp1
                 }
             }
         }
-
-        private void fillEmptyMap(gameData data)
+        private void FillEmptyMap(GameData data)
         {
             for (int i = 0; i < data.map.GetLength(0); i++)
             {
                 for (int j = 0; j < data.map.GetLength(1); j++)
                 {
-                    data.map[i, j] = new cell();
+                    data.map[i, j] = new Field();
                 }
             }
         }
-        private void RenderMap(gameData data)
+        private void RenderMap(GameData data)
         {
             Console.Clear();
 
@@ -229,6 +274,7 @@ namespace ConsoleApp1
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
                     }
+
                     if (data.cursorX == j && data.cursorY == i)
                     {
                         Console.Write($"[{data.display[j, i]}]");
@@ -237,10 +283,13 @@ namespace ConsoleApp1
                     {
                         Console.Write($" {data.display[j, i]} ");
                     }
+
                     Console.ResetColor();
                 }
+
                 Console.WriteLine();
             }
+
             Console.WriteLine("počet zbývajících min je :" + data.mineChecked);
         }
     }
